d766600798f165730e8cd0b254b4649501c14aca
Yaroslav <yaroslav.petrenko.work@gmail.com>
2022-06-09 14:39:06 +0300

    Me first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+'use stcict';
+
+// a = 5;
+// console.log(a);
+
+// const obj = {
+// 	name: (number) => number,
+// 	0: 'a',
+// 	1: 'b',
+// };
+// console.log(obj['0']);
+
+const isCheked = 5;
+const isCheked2 = 1;
+const isCheked3 = 1;
+const isCheked4 = 1;
+const isCheked5 = 5;
+
+const res = isCheked && isCheked2 && isCheked3 && isCheked4 && isCheked5;
+
+console.log(res);
